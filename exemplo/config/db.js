057ca3eabd77819fe8ab64c2c9183abba3d223e1
EdsonTiepermann <tiepermann59@gmail.com>
2024-09-12 08:43:21 -0300

    exemplo

--- exemplo/config/db.js
+++ exemplo/config/db.js
@@ -2,10 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://Edson:ezZ92XfwQyPQkYHi@cluster0.th8hv.mongodb.net/exemplo-crud-completo', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect('mongodb+srv://Edson:ezZ92XfwQyPQkYHi@cluster0.th8hv.mongodb.net/exemplo-crud-completo');
     console.log('MongoDB conectado');
   } catch (error) {
     console.error('Erro ao conectar ao MongoDB', error);
@@ -14,22 +11,3 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
-
-
-
-// const mongoose = require('mongoose');
-
-// const conn = "mongodb+srv://Edson:ezZ92XfwQyPQkYHi@cluster0.th8hv.mongodb.net/teste"
-
-// async function connect() {
-//    await mongoose.connect(conn);
-// }
-
-// connect()
-// .then(() => {
-//     console.log("Conectado ao MongoDB");
-// }).catch((err) => {
-//     console.log(err);
-// })
-
-// module.exports = mongoose
